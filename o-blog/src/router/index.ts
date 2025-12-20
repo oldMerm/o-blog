c1bb6043ebf6eadf368f1550fe3213224d2f5eac
oldmerman <1335733842@qq.com>
2025-12-20 18:56:21 +0800

    2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component2025-12-20 ai be to init the login component

--- o-blog/src/router/index.ts
+++ o-blog/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home/index.vue'
 import Article from '@/views/Article/index.vue'
+import Login from '@/views/Login/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,6 +13,10 @@ const router = createRouter({
     {
       path: '/md',
       component: Article
+    },
+    {
+      path: '/login',
+      component: Login
     }
   ],
 })
