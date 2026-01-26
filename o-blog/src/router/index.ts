5031efcee915decc1fd869441319503f99700b4d
oldmerman <1335733842@qq.com>
2026-01-26 23:05:03 +0800

    2026-1-26 初始化管理系统代码

--- o-blog/src/router/index.ts
+++ o-blog/src/router/index.ts
@@ -4,6 +4,12 @@ import Article from '@/views/Article/components/MarkdownViewer.vue'
 import Login from '@/views/Login/index.vue'
 import Manage from '@/views/Manage/index.vue'
 
+import Admin from '@/views/admin/index.vue'
+import AdminDashboard from '@/views/admin/components/AdminDashboard.vue'
+import AdminUser from '@/views/admin/components/AdminUser.vue'
+import AdminFeedback from '@/views/admin/components/AdminFeedback.vue'
+import AdminArticle from '@/views/admin/components/AdminArticle.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -26,6 +32,29 @@ const router = createRouter({
       name: 'manage',
       path: '/manage',
       component: Manage
+    },
+    {
+      name: 'admin',
+      path: '/admin',
+      component: Admin,
+      children: [
+        {
+          path: '/admin/dashboard',
+          component: AdminDashboard
+        },
+        {
+          path: '/admin/users',
+          component: AdminUser
+        },
+        {
+          path: '/admin/feedback',
+          component: AdminFeedback
+        },
+        {
+          path: '/admin/article',
+          component: AdminArticle
+        }
+      ]
     }
   ],
 })
